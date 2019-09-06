cf06e79ccadc18e5bf08ea5b93c89f4df175ad5a
Mik Pedersen <45688364+MikPedersen@users.noreply.github.com>
2019-09-06 13:57:43 +0200

    Initial commit

---
+++ src/MyClass.java
@@ -0,0 +1,25 @@
+class MyClass {
+    //psvm
+    public static void main(String[ ] args) {
+        //sout
+        System.out.println("Opgave 1.1");
+        System.out.println("Welcome to Java");
+        System.out.println("Learning Java now");
+        System.out.println("Programming is fun");
+
+        System.out.println("Opgave 1.2");
+        for (int i=0; i<5;i++){
+            System.out.println("I love Java");
+
+            System.out.println("Opgave 1.4");
+            System.out.println("a   a^2     a^3     a^4");
+            System.out.println("1   1       1       1");
+            System.out.println("2   4       8       16");
+            System.out.println("4   16      64      256");
+
+            System.out.println("Opgave 1.5");
+            double math = (7.5*6.5-4.5*3)/47.5-55;
+            System.out.println(math);
+        }
+    }
+}
